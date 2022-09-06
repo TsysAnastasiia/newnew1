6aa5f39c8ab5c4e3ebcbaca9c175616596388ef0
TsysAnastasiia <112326472+TsysAnastasiia@users.noreply.github.com>
2022-09-06 09:27:22 +0300

    Add files via upload

---
+++ Program lab 1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorldApp
+{
+
+    class Geeks
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
